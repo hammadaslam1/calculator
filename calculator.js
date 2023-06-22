9c7141405391f4ec3fd38d9c4b564a187e109c7d
Muhammad Hammad Aslam <131670890+hammadaslam1@users.noreply.github.com>
2023-06-22 19:29:39 +0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,20 @@
-const add = 
+
+      let display = document.getElementById('question');
+      let result = document.getElementById('answer');
+
+      function appendNumber(number) {
+        display.value += number;
+      }
+
+      function appendOperator(operator) {
+        display.value += operator;
+      }
+
+      function clearDisplay() {
+        display.value = '';
+        result.value = '';
+      }
+
+      function calculateResult() {
+        
+      }
