46c3dd9469cf465a755d1d0a3af4bcc11b17ee68
Muhammad Hammad Aslam <131670890+hammadaslam1@users.noreply.github.com>
2023-06-22 20:18:51 +0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,8 +3,10 @@
       let result = document.getElementById('answer');
 
       function appendNumber(number) {
+          
         display.value += number;
       }
+
 
       function appendOperator(operator) {
         display.value += operator;
@@ -16,5 +18,15 @@
       }
 
       function calculateResult() {
-        
-      }
+          try {
+            let dV = display.value
+              if(dV.indexOf('^')!==-1) {
+                dV = display.value.replace('^', '**');
+              }
+              var execute = eval(dV)
+              result.value = execute;
+            } catch (ex) {
+                result.value = "error"
+            }
+            
+        }
